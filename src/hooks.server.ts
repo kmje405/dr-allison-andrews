3c97bc599ecb5b7c44e5c3e84491c5bcc4931c6d
K. M. Jones-Eastland <kmje405@gmail.com>
2025-08-21 17:18:45 -0500

    adding sentry sdk and config

---
+++ src/hooks.server.ts
@@ -0,0 +1,21 @@
+import { sequence } from '@sveltejs/kit/hooks';
+import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+	dsn: 'https://bcfc1c70e064ac0a1cd53335b31cf303@o4509884356296704.ingest.us.sentry.io/4509884357738496',
+
+	tracesSampleRate: 1.0,
+
+	// Enable logs to be sent to Sentry
+	enableLogs: true
+
+	// uncomment the line below to enable Spotlight (https://spotlightjs.com)
+	// spotlight: import.meta.env.DEV,
+});
+
+// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+export const handle = sequence(sentryHandle());
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
